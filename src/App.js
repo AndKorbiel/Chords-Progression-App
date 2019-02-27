381e338203c1d3ab9748cfac9741f3de94dc3668
AndKorbiel <andkorbiel@gmail.com>
2019-02-27 21:14:06 +0100

    Chords display functionality beta

--- src/App.js
+++ src/App.js
@@ -4,12 +4,41 @@ import './App.css';
 const CHORDS = ['C', 'D', 'Dm', 'E', 'Em', 'E7', 'F', 'Fm', 'G', 'G7', 'A', 'Am', 'A7', 'B', 'Bm']
 
 class App extends Component {
+
+  state = {
+    pickedChords: [],
+  }
+
+  pickChords = () => {
+    let pickedRandomChords = []
+
+    for (let i = 0; i < 4; i++) {
+      pickedRandomChords.push(CHORDS[Math.floor(Math.random()*CHORDS.length)]) 
+    }
+
+    this.setState({
+      pickedChords: pickedRandomChords
+    })
+
+  }
+
+  displayPickedCHord = () => {
+    let i = 0;  
+    let pickedChords = this.state.pickedChords;
+
+    setInterval(function() {            
+        document.getElementById('player').innerHTML = '<p class="displayed-chord">'+pickedChords[i++] +'</p>';   
+        if (i == pickedChords.length) i = 0;   
+    }, 1000);    
+  }
+
   render() {
+    const { pickedChords } = this.state;
+
     return (
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12">
-            <div className="App">
+      <div className="App container">
+        <div className="row">
+          <div className="col-sm-12">
               <ul className="chords-table">
                 {CHORDS.map((chord) => {
                   return (
@@ -17,11 +46,16 @@ class App extends Component {
                   )
                 })}
               </ul>
-            </div>
+          </div>
+          <div className="col-sm-12">
+                <button className="app-button" onClick={this.pickChords}>Start</button>
+                <button className="app-button" onClick={this.displayPickedCHord}>Start</button>
+          </div>
+          <div id="player" className="col-sm-12">
+            
           </div>
         </div>
       </div>
-      
     );
   }
 }
